64af3c7dbd527fd72f8a9fe0e18ac6835d096c6f
Thushan2002 <selvakumarthushanthan4@gmail.com>
2025-11-07 19:55:31 +0530

    chore: admin UI initialised

---
+++ blog-app/app/admin/layout.jsx
@@ -0,0 +1,10 @@
+import Sidebar from "@/components/AdminComponents/Sidebar";
+
+export default function Layout({ children }) {
+  return (
+    <div className="flex">
+      <Sidebar />
+      {children}
+    </div>
+  );
+}
